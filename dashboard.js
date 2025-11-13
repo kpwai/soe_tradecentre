cea30fa83e70127825802430f44a03d702ff0389
kpwai <58467239+kpwai@users.noreply.github.com>
2025-11-13 14:08:54 +0800

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -280,10 +280,3 @@ document.getElementById("applyFilters").addEventListener("click", () => applyFil
 // INITIALIZE DASHBOARD
 // ========================================================
 loadCSV();
-
-
-
-
-
-
-
